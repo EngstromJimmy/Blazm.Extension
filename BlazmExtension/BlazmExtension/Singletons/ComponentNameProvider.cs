ba70103a9bd6ee0f24dcc7063eb41cb93b79a4b9
biegehydra <connorhallman29@gmail.com>
2023-08-30 00:06:26 -0400

    Add search capaibilities to component usages wpf component

---
+++ BlazmExtension/BlazmExtension/Singletons/ComponentNameProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazmExtension.Singletons
+{
+    public static class ComponentNameProvider
+    {
+        private static List<string> ComponentNames { get; set; } = new List<string>();
+
+        public static void SetComponentNames(IEnumerable<string> componentNames)
+        {
+            if (componentNames != null)
+            {
+                ComponentNames = componentNames.ToList();
+            }
+        }
+        public static IEnumerable<string> GetComponentNames()
+        {
+            return ComponentNames;
+        }
+    }
+}
